6ea62c0fddfad29c14897ad9e9d07e5ec9f256e3
Johan Sebastian Meneses Tovar <johan.meneses@AvalDigitalLabs.com>
2022-08-07 18:39:20 -0500

    Avance

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+
+@NgModule({
+  exports: [
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatGridListModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule
+  ]
+})
+export class MaterialModule { }
